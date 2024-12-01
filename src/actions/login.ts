2d2212cade1fdb3f3112e9dfaaded81297c48e72
chrgsoni07 <chrgsoni07@gmail.com>
2024-12-02 00:01:18 +0530

    error message

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -21,7 +21,16 @@ export const login = async (formData: FormData) => {
     });
   } catch (error) {
     if (error instanceof AuthError) {
-      console.log("Auth error");
+      console.error(error.cause?.err?.name);
+
+      console.error(error.cause?.err?.message); //Request failed with status code 401
+
+      console.error(error.name);
+
+      console.error(error.message);
+
+      console.log(" ------------------------ ");
+
       return redirect(`/signin?error=${error.type}`);
     }
     throw error;
